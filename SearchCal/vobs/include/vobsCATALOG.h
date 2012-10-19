455fc39f323b292d5114a8a8257114f7682690be
bourgesl <bourges.laurent@gmail.com>
2012-10-19 13:18:00 +0000

    added catalog attributes: posError, epochFrom, epochTo, overwritePropertyIDList) use mcs macros to remove redundant if (x == mcsFAILURE) ... code formatting

--- SearchCal/vobs/include/vobsCATALOG.h
+++ SearchCal/vobs/include/vobsCATALOG.h
@@ -21,7 +21,7 @@
 #define vobsCATALOG_CIO_ID          "II/225/catalog"
 #define vobsCATALOG_DENIS_ID        "B/denis"
 #define vobsCATALOG_DENIS_JK_ID     "J/A+A/413/1037/table1"
-#define vobsCATALOG_HIC_ID          "I/196/main"
+#define vobsCATALOG_HIC_ID          "I/196/main"                // should use I/311/hip2 = Hipparcos, the New Reduction (van Leeuwen, 2007)
 #define vobsCATALOG_LBSI_ID         "J/A+A/393/183/catalog"
 #define vobsCATALOG_MASS_ID         "II/246/out"
 #define vobsCATALOG_MERAND_ID       "J/A+A/433/1155"
@@ -29,7 +29,7 @@
 #define vobsCATALOG_PHOTO_ID        "II/7A/catalog"
 #define vobsCATALOG_SBSC_ID         "V/36B/bsc4s"
 #define vobsCATALOG_SB9_ID          "B/sb9/main"
-#define vobsCATALOG_UNSO_ID         "I/284"
+#define vobsCATALOG_USNO_ID         "I/284"
 #define vobsCATALOG_WDS_ID          "B/wds/wds"
 
 /*
@@ -57,11 +57,16 @@
 class vobsCATALOG
 {
 public:
-   // Constructor
-   vobsCATALOG(const char* name);
+    // Constructor
+    vobsCATALOG(const char* name,
+                const mcsDOUBLE posError = alxARCSEC_IN_DEGREES,
+                const mcsDOUBLE epochFrom = EPOCH_2000,
+                const mcsDOUBLE epochTo = EPOCH_2000,
+                const vobsSTAR_PROPERTY_ID_LIST* overwritePropertyIDList = NULL
+                );
 
-   // Destructor
-   virtual ~vobsCATALOG();
+    // Destructor
+    virtual ~vobsCATALOG();
 
     /**
      * Get the catalog name as string literal
@@ -73,42 +78,106 @@ public:
         return _name;
     }
 
-   /**
-    * Get the option as string literal
-    *
-    * @return option or "" if not set.
-    */
-   inline const char* GetOption() const  __attribute__((always_inline))
-   {
-       if (_option == NULL)
-       {
-           return "";
-       }
-       return _option;
-   }
+    /**
+     * Get the option as string literal
+     *
+     * @return option or "" if not set.
+     */
+    inline const char* GetOption() const __attribute__((always_inline))
+    {
+        if (_option == NULL)
+        {
+            return "";
+        }
+        return _option;
+    }
 
-   virtual mcsCOMPL_STAT SetOption(const char* option)
-   {
+    virtual mcsCOMPL_STAT SetOption(const char* option)
+    {
         _option = option;
 
         return mcsSUCCESS;
-   }
+    }
 
-   // Method to get a  star list from the catalog
-   virtual mcsCOMPL_STAT Search(vobsREQUEST &request, vobsSTAR_LIST &list, mcsLOGICAL logResult = mcsFALSE) = 0;
-   
+    // Method to get a  star list from the catalog
+    virtual mcsCOMPL_STAT Search(vobsREQUEST &request, vobsSTAR_LIST &list,
+                                 PropertyCatalogMapping* propertyCatalogMap, mcsLOGICAL logResult = mcsFALSE) = 0;
+
+    /**
+     * Get the Epoch (from) of the catalog
+     *
+     * @return Epoch (from) of the catalog
+     */
+    inline const mcsDOUBLE GetEpochFrom() const __attribute__((always_inline))
+    {
+        return _epochFrom;
+    }
+
+    /**
+     * Get the Epoch (to) of the catalog
+     *
+     * @return Epoch (to) of the catalog
+     */
+    inline const mcsDOUBLE GetEpochTo() const __attribute__((always_inline))
+    {
+        return _epochTo;
+    }
+
+    /**
+     * Get the Epoch (median) of the catalog
+     *
+     * @return Epoch (median) of the catalog
+     */
+    inline const mcsDOUBLE GetEpochMedian() const __attribute__((always_inline))
+    {
+        return _epochMed;
+    }
+
+    /**
+     * Return the flag to indicate if _epochFrom = _epochTo
+     *
+     * @return flag to indicate if _epochFrom = _epochTo
+     */
+    inline const mcsLOGICAL IsSingleEpoch() const __attribute__((always_inline))
+    {
+        return _singleEpoch;
+    }
+
+    /**
+     * Return mcsTRUE only if the catalog's epochs are equals to 2000.0
+     * @return mcsTRUE only if the catalog's epochs are equals to 2000.0
+     */
+    inline const mcsLOGICAL isEpoch2000() const __attribute__((always_inline))
+    {
+        return (_singleEpoch == mcsTRUE && _epochMed == EPOCH_2000) ? mcsTRUE : mcsFALSE;
+    }
+
 private:
-   // Declaration of assignment operator as private
-   // method, in order to hide them from the users.
-   vobsCATALOG& operator=(const vobsCATALOG&);
-   vobsCATALOG (const vobsCATALOG&);
+    // Declaration of assignment operator as private
+    // method, in order to hide them from the users.
+    vobsCATALOG& operator=(const vobsCATALOG&);
+    vobsCATALOG(const vobsCATALOG&);
 
-   // Name of the catalog
-   const char* _name;
- 
-   // options for the query string:
-   const char* _option;
+    // Name of the catalog
+    const char* _name;
 
+    // options for the query string:
+    const char* _option;
+
+    // position error of the catalog
+    mcsDOUBLE _posError;
+
+    // Epoch (from) of the catalog
+    mcsDOUBLE _epochFrom;
+    // Epoch (to) of the catalog
+    mcsDOUBLE _epochTo;
+    // Epoch (median) of the catalog
+    mcsDOUBLE _epochMed;
+    // flag to indicate if _epochFrom = _epochTo
+    mcsLOGICAL _singleEpoch;
+
+    // list of star properties that this catalog can overwrite
+    vobsSTAR_PROPERTY_ID_LIST _overwritePropertyIDList;
 };
 
 #endif /*!vobsCATALOG_H*/
