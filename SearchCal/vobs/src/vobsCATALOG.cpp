57fc8c7ef9a785b588901160b10e234b2addb220
bourgesl <bourges.laurent@gmail.com>
2012-10-19 13:26:07 +0000

    added catalog attributes: posError, epochFrom, epochTo, overwritePropertyIDList) use mcs macros to remove redundant if (x == mcsFAILURE) ... code formatting

--- SearchCal/vobs/src/vobsCATALOG.cpp
+++ SearchCal/vobs/src/vobsCATALOG.cpp
@@ -11,7 +11,7 @@
  * System Headers 
  */
 #include <iostream>
-using namespace std;  
+using namespace std;
 
 
 /*
@@ -37,12 +37,46 @@ using namespace std;
 /**
  * Build a catalog object.
  */
-vobsCATALOG::vobsCATALOG(const char* name)
+vobsCATALOG::vobsCATALOG(const char* name,
+                         const mcsDOUBLE posError,
+                         const mcsDOUBLE epochFrom,
+                         const mcsDOUBLE epochTo,
+                         const vobsSTAR_PROPERTY_ID_LIST* overwritePropertyIDList)
 {
     // Set name
     _name = name;
     // Initialize option
     _option = NULL;
+
+    // position error of the catalog
+    _posError = posError;
+
+    // Epoch (from) of the catalog
+    _epochFrom = epochFrom;
+    // Epoch (to) of the catalog
+    _epochTo = epochTo;
+
+    if (epochTo == epochFrom)
+    {
+        _singleEpoch = mcsTRUE;
+        _epochMed = _epochFrom;
+    }
+    else
+    {
+        _singleEpoch = mcsFALSE;
+        // Epoch (median) of the catalog
+        _epochMed = 0.5 * (_epochFrom + _epochTo);
+    }
+
+    // list of star properties that this catalog can overwrite
+    if (overwritePropertyIDList != NULL && overwritePropertyIDList->size() > 0)
+    {
+        for (vobsSTAR_PROPERTY_ID_LIST::const_iterator propertyIDIterator = overwritePropertyIDList->begin();
+             propertyIDIterator != overwritePropertyIDList->end(); propertyIDIterator++)
+        {
+            _overwritePropertyIDList.push_back(*propertyIDIterator);
+        }
+    }
 }
 
 
