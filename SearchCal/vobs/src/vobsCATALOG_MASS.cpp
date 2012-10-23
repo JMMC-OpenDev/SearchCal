981128f4f5cbbfd3aeabfed32bdd675f47a1f773
bourgesl <bourges.laurent@gmail.com>
2012-10-23 16:01:24 +0000

    Qflag checks only if quality is too bad (U or X) ie A to E is considered valid checks opt='T' to move Phg B / R values into JHN B / V (Tycho special case)

--- SearchCal/vobs/src/vobsCATALOG_MASS.cpp
+++ SearchCal/vobs/src/vobsCATALOG_MASS.cpp
@@ -35,14 +35,14 @@ using namespace std;
 #include "vobsErrors.h"
 
 /*
- * North: 1997 June 7 - 2000 December 1 UT
- * South: 1998 March 18 - 2001 February 15 UT
+ * North: 1997 June 7 - 2000 December 1 UT (mjd = 50606)
+ * South: 1998 March 18 - 2001 February 15 UT (mjd = 51955)
  */
 
 /*
  * Class constructor
  */
-vobsCATALOG_MASS::vobsCATALOG_MASS() : vobsREMOTE_CATALOG(vobsCATALOG_MASS_ID, true, 1.0, 1997.4, 2001.125)
+vobsCATALOG_MASS::vobsCATALOG_MASS() : vobsREMOTE_CATALOG(vobsCATALOG_MASS_ID, true, 1.0, 1997.43, 2001.13)
 {
 }
 
@@ -75,52 +75,124 @@ mcsCOMPL_STAT vobsCATALOG_MASS::ProcessList(vobsSTAR_LIST &list)
         // call parent implementation first:
         FAIL(vobsREMOTE_CATALOG::ProcessList(list));
 
-        // keep only flux whom quality is A (vobsSTAR_CODE_QUALITY property Qflg column)
-
+        // keep only flux whom quality is between (A and E) (vobsSTAR_CODE_QUALITY property Qflg column)
+        // ie ignore F, X or U flagged data
         static const char* fluxProperties[] = {vobsSTAR_PHOT_JHN_J, vobsSTAR_PHOT_JHN_H, vobsSTAR_PHOT_JHN_K};
 
+        int idIdx = -1, qFlagIdx = -1, optIdx = -1, magPhgBIdx = -1, magPhgRIdx = -1, magJhnBIdx = -1, magJhnVIdx = -1;
+        vobsSTAR_PROPERTY *qFlagProperty, *optProperty;
+        vobsSTAR_PROPERTY *magPhgBProperty, *magPhgRProperty, *magJhnBProperty, *magJhnVProperty;
         vobsSTAR* star = NULL;
-        int propIdx = -1;
-        int idIdx = -1;
-        vobsSTAR_PROPERTY* qFlagProperty;
-        const char* code;
-        const char* starId;
-        char flag;
+        const char *starId, *code, *opt;
+        char ch;
+        mcsDOUBLE mag;
 
         // For each star of the list
         for (star = list.GetNextStar(mcsTRUE); star != NULL; star = list.GetNextStar(mcsFALSE))
         {
-            if (propIdx == -1)
+            if (idIdx == -1)
             {
-                propIdx = star->GetPropertyIndex(vobsSTAR_CODE_QUALITY);
                 idIdx = star->GetPropertyIndex(vobsSTAR_ID_2MASS);
+                qFlagIdx = star->GetPropertyIndex(vobsSTAR_CODE_QUALITY);
+                optIdx = star->GetPropertyIndex(vobsSTAR_2MASS_OPT_ID_CATALOG);
+
+                magPhgBIdx = star->GetPropertyIndex(vobsSTAR_PHOT_PHG_B);
+                magPhgRIdx = star->GetPropertyIndex(vobsSTAR_PHOT_PHG_R);
+
+                magJhnBIdx = star->GetPropertyIndex(vobsSTAR_PHOT_JHN_B);
+                magJhnVIdx = star->GetPropertyIndex(vobsSTAR_PHOT_JHN_V);
             }
 
+            // Get the star ID (logs)
+            starId = star->GetProperty(idIdx)->GetValue();
+
             // Get QFlg property:
-            qFlagProperty = star->GetProperty(propIdx);
+            qFlagProperty = star->GetProperty(qFlagIdx);
 
             // test if property is set
-            if (qFlagProperty->IsSet())
+            if (qFlagProperty->IsSet() == mcsTRUE)
             {
                 code = qFlagProperty->GetValue();
 
                 if (strlen(code) == 3)
                 {
-                    // Get the star ID (logs)
-                    starId = star->GetProperty(idIdx)->GetValue();
-
                     for (int i = 0; i < 3; i++)
                     {
-                        flag = code[i];
+                        ch = code[i];
 
-                        // TODO: decide which quality use (A or ...)
-                        if (flag != 'A')
+                        // check quality between (A and E)
+                        if ((ch < 'A') && (ch > 'E'))
                         {
-                            logTest("Star '2MASS %s' - clear property %s (bad quality = '%c')", starId, fluxProperties[i], flag);
+                            logTest("Star '2MASS %s' - clear property %s (bad quality = '%c')", starId, fluxProperties[i], ch);
 
                             star->ClearPropertyValue(fluxProperties[i]);
                         }
                     }
+                }
+                else
+                {
+                    logTest("Star '2MASS %s' - invalid Qflg value '%s'", starId, code);
+                }
+            }
+
+            // filter Rmag/Bmag according to Associated optical source (opt):
+            optProperty = star->GetProperty(optIdx);
+
+            // test if property is set
+            if (optProperty->IsSet() == mcsTRUE)
+            {
+                opt = optProperty->GetValue();
+
+                if (strlen(opt) == 1)
+                {
+                    ch = opt[0];
+
+                    /* 
+                     * http://www.ipac.caltech.edu/2mass/releases/allsky/doc/sec4_4f.html
+                     * 
+                     * The optical magnitudes listed for the optical associations in the PSC are derived from the Tycho 2 and USNO-A2.0 values. 
+                     * In the case of Tycho 2 associations, the optical magnitudes listed are Johnson B and V magnitudes and are derived from 
+                     * the Tycho blue (BT) and visual (VT) magnitudes using the transformations given by (Høg et al. 2000, "Guide to the Tycho-2 Catalog"):
+                     * 
+                     * V = VT - 0.090 * (BT - VT)
+                     * B-V = 0.850 * (BT - VT)
+                     * 
+                     * For USNO-A2.0 associations, the optical magnitudes are the photographic blue and red magnitudes taken explicitly from the USNO-A2.0 Catalog.      
+                     */
+
+                    // TODO: decide if photographic blue and red magnitudes should be removed definitely (Denis, USNO, 2MASS ...)
+                    // because useless for computations only used by GUI (display)
+
+                    if (ch == 'T')
+                    {
+                        // move Tycho magnitudes into proper properties:
+
+                        // vobsSTAR_PHOT_PHG_B => vobsSTAR_PHOT_JHN_B
+                        magPhgBProperty = star->GetProperty(magPhgBIdx);
+                        magJhnBProperty = star->GetProperty(magJhnBIdx);
+
+                        if ((magPhgBProperty->IsSet() == mcsTRUE) && (magJhnBProperty->IsSet() == mcsFALSE))
+                        {
+                            FAIL(magPhgBProperty->GetValue(&mag));
+                            FAIL(magJhnBProperty->SetValue(mag, magPhgBProperty->GetOrigin()));
+                        }
+                        magPhgBProperty->ClearValue();
+
+                        // vobsSTAR_PHOT_PHG_R => vobsSTAR_PHOT_JHN_V
+                        magPhgRProperty = star->GetProperty(magPhgRIdx);
+                        magJhnVProperty = star->GetProperty(magJhnVIdx);
+
+                        if ((magPhgRProperty->IsSet() == mcsTRUE) && (magJhnVProperty->IsSet() == mcsFALSE))
+                        {
+                            FAIL(magPhgRProperty->GetValue(&mag));
+                            FAIL(magJhnVProperty->SetValue(mag, magPhgRProperty->GetOrigin()));
+                        }
+                        magPhgRProperty->ClearValue();
+                    }
+                }
+                else
+                {
+                    logTest("Star '2MASS %s' - invalid opt value '%s'", starId, opt);
                 }
             }
         }
@@ -151,6 +223,12 @@ mcsCOMPL_STAT vobsCATALOG_MASS::WriteQuerySpecificPart(void)
     // Get the Julian date of source measurement (TIME_DATE) stored in the 'vobsSTAR_JD_DATE' property
     miscDynBufAppendString(&_query, "&-out=JD");
 
+    // Get the associated optical source opt (ID_CATALOG) stored in the 'vobsSTAR_2MASS_OPT_ID_CATALOG' property
+    miscDynBufAppendString(&_query, "&-out=opt");
+
+    // Get the quality flag Qflg (CODE_QUALITY) stored in the 'vobsSTAR_CODE_QUALITY' property
+    miscDynBufAppendString(&_query, "&-out=Qflg");
+
     // Get the galactic latitude  GLAT (POS_GAL_LAT) stored in the 'vobsSTAR_POS_GAL_LAT' property
     miscDynBufAppendString(&_query, "&-out=GLAT");
 
@@ -166,17 +244,22 @@ mcsCOMPL_STAT vobsCATALOG_MASS::WriteQuerySpecificPart(void)
     // Get the johnson magnitude Kmag (PHOT_JHN_K) stored in the 'vobsSTAR_PHOT_JHN_K' property
     miscDynBufAppendString(&_query, "&-out=Kmag");
 
-    // Get the photometric magnitude Rmag (PHOT_PHG_R) stored in the 'vobsSTAR_PHOT_PHG_R' property
-    miscDynBufAppendString(&_query, "&-out=Rmag");
+    /* 
+     * The optical magnitudes listed for the optical associations in the PSC are derived from the Tycho 2 and USNO-A2.0 values. 
+     * In the case of Tycho 2 associations, the optical magnitudes listed are Johnson B and V magnitudes and are derived from 
+     * the Tycho blue (BT) and visual (VT) magnitudes using the transformations given by (Høg et al. 2000, "Guide to the Tycho-2 Catalog").
+     * 
+     * For USNO-A2.0 associations, the optical magnitudes are the photographic blue and red magnitudes taken explicitly from the USNO-A2.0 Catalog.      
+     */
+
+    // TODO: decide if photographic blue and red magnitudes should be removed definitely (Denis, USNO, 2MASS ...)
+    // because useless for computations only used by GUI (display)
 
     // Get the photometric magnitude Bmag (PHOT_PHG_B) stored in the 'vobsSTAR_PHOT_PHG_B' property
     miscDynBufAppendString(&_query, "&-out=Bmag");
 
-    // Get the associated optical source opt (ID_CATALOG) stored in the 'vobsSTAR_ID_CATALOG' property
-    miscDynBufAppendString(&_query, "&-out=opt");
-
-    // Get the quality flag Qflg (CODE_QUALITY) stored in the 'vobsSTAR_CODE_QUALITY' property
-    miscDynBufAppendString(&_query, "&-out=Qflg");
+    // Get the photometric magnitude Rmag (PHOT_PHG_R) stored in the 'vobsSTAR_PHOT_PHG_R' property
+    miscDynBufAppendString(&_query, "&-out=Rmag");
 
     return mcsSUCCESS;
 }
@@ -202,9 +285,7 @@ mcsCOMPL_STAT vobsCATALOG_MASS::WriteQuerySpecificPart(vobsREQUEST &request)
 
     // Add the magnitude range constraint
     mcsSTRING32 rangeMag;
-    mcsDOUBLE minMagRange = request.GetMinMagRange();
-    mcsDOUBLE maxMagRange = request.GetMaxMagRange();
-    sprintf(rangeMag, "%.2lf..%.2lf", minMagRange, maxMagRange);
+    sprintf(rangeMag, "%.2lf..%.2lf", request.GetMinMagRange(), request.GetMaxMagRange());
 
     const char* geomParam;
     mcsSTRING32 separation;
@@ -214,9 +295,7 @@ mcsCOMPL_STAT vobsCATALOG_MASS::WriteQuerySpecificPart(vobsREQUEST &request)
     {
         geomParam = "&-c.geom=b&-c.bm="; // -c.bm means box in arcmin
 
-        mcsDOUBLE deltaRa;
-        mcsDOUBLE deltaDec;
-
+        mcsDOUBLE deltaRa, deltaDec;
         FAIL(request.GetSearchArea(deltaRa, deltaDec));
 
         sprintf(separation, "%.0lf/%.0lf", deltaRa, deltaDec);
@@ -226,7 +305,6 @@ mcsCOMPL_STAT vobsCATALOG_MASS::WriteQuerySpecificPart(vobsREQUEST &request)
         geomParam = "&-c.rm="; // -c.rm means radius in arcmin
 
         mcsDOUBLE radius;
-
         FAIL(request.GetSearchArea(radius));
 
         sprintf(separation, "%.0lf", radius);
