a1ef0221831ff9702a704f9c47b291cbd5bf9a86
bourgesl <bourges.laurent@gmail.com>
2013-06-24 13:21:44 +0000

    added missing sclsvrErrors, sclsvrGETSTAR_CMD and sclsvrGETCAL_CMD.h header file

---
+++ SearchCal/sclsvr/include/sclsvrGETSTAR_CMD.h
@@ -0,0 +1,74 @@
+/*
+ * 
+ * This file has been automatically generated
+ * 
+ * !!!!!!!!!!!  DO NOT MANUALLY EDIT THIS FILE  !!!!!!!!!!!
+ */
+#ifndef sclsvrGETSTAR_CMD_H
+#define sclsvrGETSTAR_CMD_H
+
+/**
+ * \file
+ * Generated for sclsvrGETSTAR_CMD class declaration.
+ * This file has been automatically generated. If this file is missing in your
+ * modArea, just type make all to regenerate.
+ */
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+/*
+ * MCS Headers
+ */
+#include "cmd.h"
+
+/*
+ * Command name definition
+ */
+#define sclsvrGETSTAR_CMD_NAME "GETSTAR"
+
+/*
+ * Command definition file
+ */
+#define sclsvrGETSTAR_CDF_NAME "sclsvrGETSTAR.cdf"
+
+/*
+ * Class declaration
+ */
+        
+    
+/**
+ * This class is intented to be used for a
+ * reception of the GETSTAR command 
+ */
+
+class sclsvrGETSTAR_CMD: public cmdCOMMAND
+{
+public:
+    sclsvrGETSTAR_CMD(string name, string params);
+    virtual ~sclsvrGETSTAR_CMD();
+
+
+    virtual mcsCOMPL_STAT GetObjectName(char **_objectName_);
+    virtual mcsCOMPL_STAT GetFile(char **_file_);
+    virtual mcsCOMPL_STAT GetBaseline(mcsDOUBLE *_baseline_);
+    virtual mcsLOGICAL HasDefaultBaseline(void);
+    virtual mcsCOMPL_STAT GetDefaultBaseline(mcsDOUBLE *_baseline_);
+    virtual mcsCOMPL_STAT GetWlen(mcsDOUBLE *_wlen_);
+    virtual mcsLOGICAL HasDefaultWlen(void);
+    virtual mcsCOMPL_STAT GetDefaultWlen(mcsDOUBLE *_wlen_);
+
+protected:
+
+private:
+    // Declaration of copy constructor and assignment operator as private
+    // methods, in order to hide them from the users.
+     sclsvrGETSTAR_CMD(const sclsvrGETSTAR_CMD&);
+     sclsvrGETSTAR_CMD& operator=(const sclsvrGETSTAR_CMD&);
+
+};
+
+#endif /*!sclsvrGETSTAR_CMD_H*/
+
+/*___oOo___*/
