6e9eab87255f9a4f700a98bda9e74fe9df47db86
bourgesl <bourges.laurent@gmail.com>
2013-06-26 15:57:58 +0000

    added script to filter and compare JSDC catalogs

--- SearchCal/sclsvr/test/JSDC/filter.sh
+++ SearchCal/sclsvr/test/JSDC/filter.sh
@@ -2,4 +2,13 @@
 #*******************************************************************************
 # JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
 #*******************************************************************************
-rm *.log *.vot *.out *.fits
+
+CURRENT=`pwd`
+
+cd ../../../sclcat/bin/
+./sclcatFilterCatalog $CURRENT/catalog/ $1
+
+rm $CURRENT/catalog/result/catalog*.fits
+
+cp $CURRENT/catalog/result/final.fits $CURRENT/final.fits
+
