a7d4a2a29f71d8438369c9595d547208d1c69adc
bourgesl <bourges.laurent@gmail.com>
2013-07-19 13:36:48 +0000

    fixed sclsvr parameters (removed visErr, added outputFormat 2013.7)

--- SearchCal/sclsvr/test/JSDC/runJSDC.sh
+++ SearchCal/sclsvr/test/JSDC/runJSDC.sh
@@ -24,7 +24,7 @@ MON_PID=$!
 echo "monitor started: $MON_PID"
 
 # use dec=+90:00 to get catalog sorted by distance to north pole
-sclsvrServer -v 3 GETCAL "-wlen 2.2 -minMagRange -5.0 -file jsdc${suffix}.vot -objectName toto -diffRa 3600 -ra +00:00:00.000 -noScienceStar false -band 0 -bright true -diffDec 1200 -baseMax 102.45 -maxMagRange 20.0 -mag 6 -dec +90:00:00.000 -visErr 0.5" &> runJSDC${suffix}.log
+sclsvrServer -v 3 GETCAL "-wlen 2.2 -minMagRange -5.0 -file jsdc${suffix}.vot -objectName toto -diffRa 3600 -ra +00:00:00.000 -noScienceStar false -band 0 -bright true -diffDec 1200 -baseMax 102.45 -maxMagRange 20.0 -mag 6 -dec +90:00:00.000 -outputFormat 2013.7" &> runJSDC${suffix}.log
 
 # kill monitor
 echo -n "monitor stopping ..."
