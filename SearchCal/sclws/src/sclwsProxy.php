032ecbab58e293a38ae2842d4b7ebf38aad2b859
bourgesl <bourges.laurent@gmail.com>
2013-08-05 15:58:27 +0000

    fixed GetServerStatus WS name

--- SearchCal/sclws/src/sclwsProxy.php
+++ SearchCal/sclws/src/sclwsProxy.php
@@ -80,7 +80,7 @@ $soapSvrStatusMsg = <<<EOM
 <?xml version="1.0" encoding="UTF-8"?>
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
  <soapenv:Body>
-  <GetServerStatusSearchCal xmlns="urn:sclws"/>
+  <GetServerStatus xmlns="urn:sclws"/>
  </soapenv:Body>
 </soapenv:Envelope>
 EOM;
