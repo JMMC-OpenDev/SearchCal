2584256bd04473e2461f101ea4b6647a243060b8
bourgesl <bourges.laurent@gmail.com>
2014-04-22 09:42:45 +0000

    use more pessimistic threshold on max_residuals (5) and diam_chi2 (25)

--- SearchCal/alx/include/alx.h
+++ SearchCal/alx/include/alx.h
@@ -148,11 +148,11 @@ typedef struct
 /** number of sigma to log individual diameter (3 sigma) */
 #define LOG_RESIDUAL_THRESHOLD 3.0
 
-/** number of sigma to consider a diameter as inconsistent (10 sigma) */
-#define MAX_RESIDUAL_THRESHOLD 10.0
+/** number of sigma to consider a diameter as inconsistent (5 sigma) */
+#define MAX_RESIDUAL_THRESHOLD 5.0
 
-/** chi2 threshold to consider diameters as inconsistent (100.0) */
-#define DIAM_CHI2_THRESHOLD   100.0
+/** chi2 threshold to consider diameters as inconsistent (25.0) */
+#define DIAM_CHI2_THRESHOLD   25.0
 
 #define alxNB_SED_BAND 5
 
--- SearchCal/alx/src/alxAngularDiameter.c
+++ SearchCal/alx/src/alxAngularDiameter.c
@@ -1049,7 +1049,7 @@ mcsCOMPL_STAT alxComputeMeanAngularDiameter(alxDIAMETERS diameters,
                 }
             }
 
-            /* Check if max(residuals) < 10 or chi2 < 100
+            /* Check if max(residuals) < 5 or chi2 < 25
              * If higher i.e. inconsistency is found, the weighted mean diameter has a LOW confidence */
             if ((maxResidual > MAX_RESIDUAL_THRESHOLD) || (chi2 > DIAM_CHI2_THRESHOLD))
             {
