c6e8a658a7ce4cb699fd2dd3eeb8d2edc0996214
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2016-02-18 15:05:51 +0000

    improve log, enable coredump and add new 'verify' service target call by crontab

--- SearchCal/sclws/src/sclwsManager.sh
+++ SearchCal/sclws/src/sclwsManager.sh
@@ -31,79 +31,104 @@ if [ "`uname`" = "Linux" ]; then enable -n echo; fi
 # Source function library.
 . /etc/rc.d/init.d/functions
 
-gprintf()
+_log()
 {
-echo $*
+  echo "$*"
+  logger -t "${processId}-${processSoapPort}" "$*"
 }
-# uncomment next line to accept some cores
-# ulimit -c unlimited
+_log_error()
+{
+  logger -s -t "${processId}-${processSoapPort}" "$*"
+}
+
 
 # Define some usefull constants program name and process id
 prog="sclwsServer -v 3"
 processId="sclwsServer"
+processSoapPort="8079"
+logFile=/var/log/${processId}.log
 userName="sclws"
 PID_FILE="/var/run/${prog}.pid"
+
+# 1M for max coredump size
+DAEMON_COREFILE_LIMIT=1024 
+# make sure it doesn't core dump anywhere unless requested
+corelimit="ulimit -S -c ${DAEMON_COREFILE_LIMIT:-0}"
+
+# used to gracefully stop the process (one checkpoint is tested each second for a faster restart)
+let WAITINGPAUSE=10
+
 
 #handle parameter
 case "$1" in
   start)
-  	gprintf "Starting %s: " "$prog"
-    echo
+  	_log "Starting $prog :"
    
     if [ `pidofproc -p "{$PID_FILE}" "${processId}"` ]
-       then
-           gprintf "Sorry, %s is already running" "$prog"
-           failure
-       else
-			logFile=/var/log/${processId}.log
+    then
+      _log_error "Sorry, $prog is already running"
+      failure
+    else
+      
 			mv $logFile ${logFile}.$(date +"%F_%T") 
 
 			# start real process and keep pid trace
-       		su - ${userName} -c "${prog} &" &> ${logFile}			
+       		su - ${userName} -c "$corelimit >/dev/null 2>&1 ; ${prog} &" &> ${logFile}			
        		ps -ef |grep -e "^${userName}"|grep sclwsServer|awk '{print $2}' > "${PID_FILE}"
        		if [ $? -eq 0 ]
            	then
            		success 
            	else
+              _log_error "service can't be started ($prog)"
            		failure
            	fi
-       fi
-    echo
+    fi
 	;;
+
   stop)
-  let WAITINGPAUSE=10
-	gprintf "Shutting down (in max ${WAITINGPAUSE}s) %s: " "$prog"
-# kill term + loop to finish as soon as possible the stop action (with maximum delay before -9 signal)
-  
-  SC_PID=$(cat "${PID_FILE}")
-  kill $SC_PID
-  while [ $WAITINGPAUSE -gt 0 -a $(ps -u ${userName} | grep $SC_PID | wc -l) -eq 1 ]
-  do
-  let WAITINGPAUSE-=1
-  sleep 1 
-  done
-  
-  if [ $WAITINGPAUSE -eq 0 ]
-  then
-      echo killproc sent to  "${PID_FILE}" "${processId}"
-      killproc -p "${PID_FILE}" "${processId}"
-  fi
-
-
-	echo
+    _log "Shutting down (in max ${WAITINGPAUSE}s) $prog :"
+    # kill term + loop to finish as soon as possible the stop action (with maximum delay before -9 signal)
+    
+    SC_PID=$(cat "${PID_FILE}")
+    kill $SC_PID
+    while [ $WAITINGPAUSE -gt 0 -a $(ps -u ${userName} | grep $SC_PID | wc -l) -eq 1 ]
+    do
+    let WAITINGPAUSE-=1
+    sleep 1 
+    done
+    
+    if [ $WAITINGPAUSE -eq 0 ]
+    then
+        _log_error "timeout expired, killproc sent to PID=${PID_FILE}"
+        killproc -p "${PID_FILE}" "${processId}"
+    else
+        _log "program shutdown gracefully"
+    fi
 	;;
+
   status)
-	status -p "${PID_FILE}" "${processId}"
+    status -p "${PID_FILE}" "${processId}"
 	;;
+
   restart)
-  	gprintf "Re" 
-  	gprintf "Starting %s: " "$prog"
-	echo
-        $0 stop
-	$0 start
+  	_log "ReStarting $prog :"
+    $0 stop
+    $0 start
 	;;
+
+  verify)
+    if curl localhost:${processSoapPort} 2>/dev/null |grep "method not implemented" &> /dev/null 
+    then 
+       _log "service ${processId} PRODUCTION is running properly (pid="$( cat "${PID_FILE}")" soapPort=${processSoapPort} username=${userName} logfile=${logFile})"
+    else
+       _log_error "service ${processId} PRODUCTION verification failed, resting a new one"
+      $0 stop
+      $0 start
+    fi
+	;;
+
   *)
-	gprintf "*** Usage: %s {start|stop|status|restart}\n" "$prog"
+	_log "Usage: %s {start|stop|status|restart|verify}\n" "$prog"
 	exit 1
 esac
 
