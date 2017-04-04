4d9994f80395c941c8a12e39d6d3414f3c08ac7f
bourgesl <bourges.laurent@gmail.com>
2017-04-04 15:34:19 +0000

    removed dead code

--- SearchCal/sclsvr/src/sclsvrSCENARIO_JSDC.cpp
+++ SearchCal/sclsvr/src/sclsvrSCENARIO_JSDC.cpp
@@ -104,12 +104,6 @@ mcsCOMPL_STAT sclsvrSCENARIO_JSDC::Init(vobsSCENARIO_RUNTIME &ctx, vobsREQUEST* 
     // DENIS_JK - J-K DENIS photometry of bright southern stars (Kimeswenger+ 2004)
     FAIL(AddEntry(vobsCATALOG_DENIS_JK_ID, &_request, &_starList, &_starList, vobsUPDATE_ONLY, &_criteriaListRaDec));
 
-    // LBSI
-    // FAIL(AddEntry(vobsCATALOG_LBSI_ID, &_request, &_starList, &_starList, vobsUPDATE_ONLY, &_criteriaListRaDec));
-
-    // MERAND
-    // FAIL(AddEntry(vobsCATALOG_MERAND_ID, &_request, &_starList, &_starList, vobsUPDATE_ONLY, &_criteriaListRaDec));
-
     // II/7A - UBVRIJKLMNH Photoelectric Catalogue
     FAIL(AddEntry(vobsCATALOG_PHOTO_ID, &_request, &_starList, &_starList, vobsUPDATE_ONLY, &_criteriaListRaDecMagV));
 
