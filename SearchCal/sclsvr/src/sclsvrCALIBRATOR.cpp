4bc4798db51db377ed0132243710ec3c9645cc33
bourgesl <bourges.laurent@gmail.com>
2020-09-29 12:45:29 +0000

    added column XM_SIMBAD_SEP for candidate catalog (ASCC local)

--- SearchCal/sclsvr/src/sclsvrCALIBRATOR.cpp
+++ SearchCal/sclsvr/src/sclsvrCALIBRATOR.cpp
@@ -40,8 +40,8 @@ using namespace std;
 /** flag to enable / disable SED Fitting in development mode */
 #define sclsvrCALIBRATOR_PERFORM_SED_FITTING false
 
-/* maximum number of properties (118) */
-#define sclsvrCALIBRATOR_MAX_PROPERTIES 118
+/* maximum number of properties (119) */
+#define sclsvrCALIBRATOR_MAX_PROPERTIES 119
 
 /* Error identifiers */
 #define sclsvrCALIBRATOR_PHOT_COUS_J_ERROR  "PHOT_COUS_J_ERROR"
