a0cafce911b13b31745ab3f0a92ef232c1395b6d
bourgesl <bourges.laurent@gmail.com>
2024-06-13 15:54:34 +0200

    fixed parsing simbad response

--- SearchCal/simcli/src/simcliGetCoordinates.c
+++ SearchCal/simcli/src/simcliGetCoordinates.c
@@ -31,6 +31,10 @@
 #include "simcli.h"
 #include "simcliPrivate.h"
 
+
+/* trace flag */
+#define TRACE   0
+
 /*
  * Public functions definition
  */
@@ -38,32 +42,55 @@ mcsCOMPL_STAT simcliGetCoordinates(char *name,
                                    char *ra, char *dec,
                                    mcsDOUBLE *pmRa, mcsDOUBLE *pmDec,
                                    char *spType, char *objTypes,
-                                   char* mainId)
+                                   char *mainId)
 {
     miscDYN_BUF result;
     miscDynBufInit(&result);
     miscDYN_BUF url;
     miscDynBufInit(&url);
 
-    char * encodedName = miscUrlEncode(name);
+    /* reset outputs */
+    strcpy(ra, "\0");
+    strcpy(dec, "\0");
+    *pmRa = 0.0;
+    *pmDec = 0.0;
+    strcpy(spType, "\0");
+    strcpy(objTypes, "\0");
+    strcpy(mainId, "\0");
 
-    miscDynBufAppendString(&url,"http://simbad.cds.unistra.fr/simbad/sim-script?submit=submit+script&script=%23+only+data%0D%0Aoutput+console%3Doff+script%3Doff+%0D%0A%0D%0A%23+ask+for%3A%0D%0A%23+ra%2C+dec%2C+%26pmRa%2C+%26pmDec%2C+spType%2C+objTypes%2C+mainId%0D%0A%0D%0Aformat+object+form1+%22%25COO%28A%29%3B%25COO%28D%29%3B%25PM%28A%3BD%29%3B%25SP%28S%29%3B%25OTYPELIST%3B%25MAIN_ID%22%0D%0Aquery+id+");
-    miscDynBufAppendString(&url,encodedName);
+    /* Replace '_' by ' ' */
+    char starName[256];
+    char* p;
+    strncpy(starName, name, sizeof (starName) - 1);
+    p = strchr(starName, '_');
+    while (p != NULL)
+    {
+        *p = ' ';
+        p = strchr(starName, '_');
+    }
+
+    char* encodedName = miscUrlEncode(starName);
+
+    miscDynBufAppendString(&url, "http://simbad.cds.unistra.fr/simbad/sim-script?submit=submit+script&script=%23+only+data%0D%0Aoutput+console%3Doff+script%3Doff+%0D%0A%0D%0A%23+ask+for%3A%0D%0A%23+ra%2C+dec%2C+%26pmRa%2C+%26pmDec%2C+spType%2C+objTypes%2C+mainId%0D%0A%0D%0Aformat+object+form1+%22%25COO%28d%3BA%29%3B%25COO%28d%3BD%29%3B%25PM%28A%3BD%29%3B%25SP%28S%29%3B%25OTYPELIST%3B%25MAIN_ID%3B%22%0D%0Aquery+id+");
+    miscDynBufAppendString(&url, encodedName);
     free(encodedName);
 
-    /* printf("querying simbad ... (%s)\n",miscDynBufGetBuffer(&url)); */
+    if (TRACE) printf("querying simbad ... (%s)\n", miscDynBufGetBuffer(&url));
 
-    if (miscPerformHttpGet(miscDynBufGetBuffer(&url), &result, 0) == mcsFAILURE){
+    if (miscPerformHttpGet(miscDynBufGetBuffer(&url), &result, 0) == mcsFAILURE)
+    {
         errCloseStack();
         return mcsFAILURE;
     }
 
-    char * response = miscDynBufGetBuffer(&result);
-    /* printf(":\n%s", response ); */
+    char* response = miscDynBufGetBuffer(&result);
+    if (TRACE) printf("Response:\n%s\n---\n", response);
 
     /* fails if sim-script outputs :error:::: */
-    if ( IS_NOT_NULL( strstr(response, ":error:") ) ){
+    if (IS_NOT_NULL(strstr(response, ":error:")))
+    {
         errCloseStack();
+        miscDynBufDestroy(&result);
         return mcsFAILURE;
     }
 
@@ -71,34 +98,69 @@ mcsCOMPL_STAT simcliGetCoordinates(char *name,
     int fieldIndex = 0;
 
     token = strtok(response, ";");
-    while (token != NULL) {
-        switch (fieldIndex) {
-            case 0:
-                strcpy(ra, token);
-                break;
-            case 1:
-                strcpy(dec, token);
-                break;
-            case 2:
-                *pmRa = atof(token);
-                break;
-            case 3:
-                *pmDec = atof(token);
-                break;
-            case 4:
-                strcpy(spType, token);
-                break;
-            case 5:
-                strcpy(objTypes, token);
-                break;
-            case 6:
-                strcpy(mainId, token);
-                break;
-            default:
-                errCloseStack();
-                miscDynBufDestroy(&result);
+    while (token != NULL)
+    {
+        /* skip blanking values */
+        if (token[0] != '~')
+        {
+            if (TRACE) printf("parsing field[%d]: '%s'\n", fieldIndex, token);
 
-                return mcsFAILURE;
+            switch (fieldIndex)
+            {
+                case 0:
+                    mcsDOUBLE ss;
+                    sscanf(token, "%lf", &ss);
+                    ss = ss / 15.0;
+                    int HH = ss;
+                    int MM = ((ss - HH)*60.0);
+                    mcsDOUBLE SS = (ss - HH)*3600 - (MM * 60);
+                    sprintf(ra, "%02d %02d %010.7lf", HH, MM, SS);
+                    break;
+                case 1:
+                    mcsDOUBLE ds;
+                    sscanf(token, "%lf", &ds);
+                    char sign = (ds < 0.0) ? '-' : '+';
+                    ds = (ds < 0.0) ? -ds : ds;
+                    int DH = ds;
+                    int DM = ((ds - DH)*60.0);
+                    mcsDOUBLE DS = (ds - DH)*3600 - (DM * 60);
+                    sprintf(dec, "%c%d %02d %09.6f", sign, DH, DM, DS);
+                    break;
+                case 2:
+                    *pmRa = atof(token);
+                    break;
+                case 3:
+                    *pmDec = atof(token);
+                    break;
+                case 4:
+                    strcpy(spType, token);
+                    break;
+                case 5:
+                    strcpy(objTypes, ",");
+                    strcat(objTypes, token);
+                    strcat(objTypes, ",");
+                    break;
+                case 6:
+                    /* trim space character (left/right) */
+                    /* get the first token */
+                    char *tok = strtok(token, " ");
+
+                    /* walk through other tokens */
+                    while (tok != NULL)
+                    {
+                        if (strlen(mainId) != 0)
+                        {
+                            strcat(mainId, " ");
+                        }
+                        strcat(mainId, tok);
+
+                        tok = strtok(NULL, " ");
+                    }
+                    break;
+                default:
+                    miscDynBufDestroy(&result);
+                    return mcsFAILURE;
+            }
         }
         token = strtok(NULL, ";");
         fieldIndex++;
@@ -107,7 +169,4 @@ mcsCOMPL_STAT simcliGetCoordinates(char *name,
     miscDynBufDestroy(&result);
     return mcsSUCCESS;
 }
-
-
-
 /*___oOo___*/
