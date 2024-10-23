86acbab8468b9af154374b1750835f6d563ed08f
bourgesl <bourges.laurent@gmail.com>
2024-10-23 16:39:08 +0200

    fixed building JSDC

--- SearchCal/sclsvr/test/JSDC/runJSDC.sh
+++ SearchCal/sclsvr/test/JSDC/runJSDC.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 #*******************************************************************************
 # JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
 #*******************************************************************************
 
 # bright or faint JSDC scenario:
-export BRIGHT=false
+export BRIGHT=true
 
 export VOBS_VIZIER_URI="http://vizier.u-strasbg.fr"
 #export VOBS_VIZIER_URI="http://viz-beta.u-strasbg.fr"
+
 # define dev flag for development features (noticeably: use of internal database in MCSDATA):
 export VOBS_DEV_FLAG="true"
-#where is MCSDATA:
-export MCSDATA=$HOME/MCSDATA
+# disable dev flag
+export VOBS_LOW_MEM_FLAG="false"
+
+# Where is MCSDATA:
+#export MCSDATA=$HOME/MCSDATA
 
 export http_proxy=
 export https_proxy=
@@ -21,21 +25,27 @@ if [ -n "$1" ]
 then
     suffix="$1"
 fi
+
+JSDC_FILE=jsdc${suffix}.vot
+
 #export G_SLICE=always-malloc
 #export GLIBCXX_FORCE_NEW=1
 
-./monitor.sh sclsvrServer &> monitor${suffix}.log &
-
+#./monitor.sh sclsvrServer &> monitor${suffix}.log &
 # Remember monitor PID for later kill
-MON_PID=$!
-echo "monitor started: $MON_PID"
+#MON_PID=$!
+#echo "monitor started: $MON_PID"
 
+echo "Running sclsvr to produce the catalog '$JSDC_FILE' ..."
 # use dec=+90:00 to get catalog sorted by distance to north pole
-sclsvrServer -v 3 GETCAL "-wlen 2.2 -minMagRange -5.0 -file jsdc${suffix}.vot -objectName JSDC_V2 -diffRa 3600 -ra +00:00:00.000 -noScienceStar false -band 0 -bright $BRIGHT -diffDec 1200 -baseMax 102.45 -maxMagRange 20.0 -mag 6 -dec +90:00:00.000 -outputFormat 2013.7" &> runJSDC${suffix}.log
+sclsvrServer -v 3 GETCAL "-wlen 2.2 -minMagRange -5.0 -file $JSDC_FILE -objectName JSDC_V3 -ra +00:00:00.000 -dec +90:00:00.000 -noScienceStar false -band 0 -bright $BRIGHT -diffRa 3600 -diffDec 1200 -baseMax 999.0 -maxMagRange 20.0 -mag 5 -outputFormat 2013.7" &> runJSDC${suffix}.log
 
 # kill monitor
-echo -n "monitor stopping ..."
-kill $MON_PID 
+#echo -n "monitor stopping ..."
+#kill $MON_PID
+
 echo "JSDC done."
 echo "to compare outputs, remove version dependent parts with, e.g:"
 echo "cat runJSDC.log |grep -v Info |sed -e 's%- "`date +%F`"T[0123456789\:\.]*%%g' |sed -e 's%\.c.*\:[0-9]*%%g' > file_to_be_compared"
+
+# --- EoF ---
