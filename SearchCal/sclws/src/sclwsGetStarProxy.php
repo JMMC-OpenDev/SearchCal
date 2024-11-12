8045160d54d759ccda8912179f54f2c5b79d22e3
bourgesl <bourges.laurent@gmail.com>
2024-11-12 17:45:35 +0100

    added forceUpdate parameter

--- SearchCal/sclws/src/sclwsGetStarProxy.php
+++ SearchCal/sclws/src/sclwsGetStarProxy.php
@@ -56,6 +56,7 @@ if (empty($format)
 }
 
 // Parse advanced parameters:
+$forceUpdate = getParam('forceUpdate');
 $uV = getParam('V');
 $ue_V = getParam('e_V');
 $uJ = getParam('J');
@@ -67,6 +68,9 @@ $ue_K = getParam('e_K');
 $uSP_TYPE = getParam('SP_TYPE');
 
 $adv_parameters = "";
+if (!empty($forceUpdate)) {
+    $adv_parameters = $adv_parameters . " -forceUpdate " . $forceUpdate;
+}
 if (!empty($uV)) {
     $adv_parameters = $adv_parameters . " -V " . $uV;
 }
