a29895741a36cb727f2a2aab1167e343123a2e3f
mellag <guillaume.mella@univ-grenoble-alpes.fr>
2025-07-16 15:36:24 +0200

    set better name for the bibtaged file

--- SearchCal/alx/config/JMDC/scriptHandleLastVersionOf_JMDC_dot_xls_ToProduceJMDC_dot_fits.sh
+++ SearchCal/alx/config/JMDC/scriptHandleLastVersionOf_JMDC_dot_xls_ToProduceJMDC_dot_fits.sh
@@ -129,7 +129,7 @@ gdl -e "make_jsdc_script_simple,\"${NAME}_final_lddUpdated.fits\",/nopause"
 
 # Add OLBIN bibdb tags
 JMDC_BIBTAGS_IN=${NAME}_final_lddUpdated.fits
-JMDC_BIBTAGS=${JMDC_FINAL}.bibtags.fits
+JMDC_BIBTAGS=${NAME}_final_lddUpdated_pubOlbinTags.fits
 curl https://bibdbmgr.jmmc.fr/e_xml.csv.xql -o bibolbin.csv
 stilts $FLAGS tmatch2 in1="${JMDC_BIBTAGS_IN}" ifmt1=fits in2="bibolbin.csv" ifmt2="csv(header=true)" omode=out out="${JMDC_BIBTAGS}" ofmt=fits find=best1 fixcols=dups join=all1 matcher=exact values1="BIBCODE" values2="BIBCODE"
 stilts $FLAGS tpipe  ifmt=fits omode=out ofmt=fits out="${JMDC_BIBTAGS}" in="${JMDC_BIBTAGS}" cmd="colmeta -name BIBCODE BIBCODE_1" cmd="delcols 'BIBCODE_2 GroupID GroupSize'"
